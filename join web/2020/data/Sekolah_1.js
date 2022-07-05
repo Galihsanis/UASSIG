c668a0208089c5806fd8b1cc8c342c41ca80efd1
Galihsanis <108760657+Galihsanis@users.noreply.github.com>
2022-07-06 02:03:42 +0700

    Add files via upload

---
+++ join web/2020/data/Sekolah_1.js
@@ -0,0 +1,26 @@
+var json_Sekolah_1 = {
+"type": "FeatureCollection",
+"name": "Sekolah_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Swasta": "111", "Negeri": "1161" }, "geometry": { "type": "Point", "coordinates": [ 110.619768062415858, -7.149053679475041 ] } },
+{ "type": "Feature", "properties": { "Swasta": "2386", "Negeri": "2418" }, "geometry": { "type": "Point", "coordinates": [ 110.754700927212696, -7.115102829622935 ] } },
+{ "type": "Feature", "properties": { "Swasta": "213", "Negeri": "1368" }, "geometry": { "type": "Point", "coordinates": [ 110.880057911282009, -7.084634118217198 ] } },
+{ "type": "Feature", "properties": { "Swasta": "349", "Negeri": "1909" }, "geometry": { "type": "Point", "coordinates": [ 110.94883014559781, -7.165593837095297 ] } },
+{ "type": "Feature", "properties": { "Swasta": "333", "Negeri": "1715" }, "geometry": { "type": "Point", "coordinates": [ 110.970593510887625, -7.224790190683585 ] } },
+{ "type": "Feature", "properties": { "Swasta": "1082", "Negeri": "1703" }, "geometry": { "type": "Point", "coordinates": [ 111.050682695154123, -7.146442075640263 ] } },
+{ "type": "Feature", "properties": { "Swasta": "754", "Negeri": "1733" }, "geometry": { "type": "Point", "coordinates": [ 111.129030810197449, -7.159500094814151 ] } },
+{ "type": "Feature", "properties": { "Swasta": "200", "Negeri": "1633" }, "geometry": { "type": "Point", "coordinates": [ 111.20041464834803, -7.175169717822815 ] } },
+{ "type": "Feature", "properties": { "Swasta": "476", "Negeri": "1328" }, "geometry": { "type": "Point", "coordinates": [ 111.169945936942298, -7.064611822150572 ] } },
+{ "type": "Feature", "properties": { "Swasta": "525", "Negeri": "1856" }, "geometry": { "type": "Point", "coordinates": [ 111.087245148841006, -7.058518079869424 ] } },
+{ "type": "Feature", "properties": { "Swasta": "178", "Negeri": "1223" }, "geometry": { "type": "Point", "coordinates": [ 111.001062222293356, -7.064611822150572 ] } },
+{ "type": "Feature", "properties": { "Swasta": "399", "Negeri": "1223" }, "geometry": { "type": "Point", "coordinates": [ 110.958406026325321, -6.997580657057952 ] } },
+{ "type": "Feature", "properties": { "Swasta": "0", "Negeri": "717" }, "geometry": { "type": "Point", "coordinates": [ 110.891374861232705, -7.008027072397062 ] } },
+{ "type": "Feature", "properties": { "Swasta": "0", "Negeri": "649" }, "geometry": { "type": "Point", "coordinates": [ 110.834790111479194, -7.011509210843432 ] } },
+{ "type": "Feature", "properties": { "Swasta": "205", "Negeri": "1448" }, "geometry": { "type": "Point", "coordinates": [ 110.7886517770648, -7.047201129918722 ] } },
+{ "type": "Feature", "properties": { "Swasta": "1251", "Negeri": "2138" }, "geometry": { "type": "Point", "coordinates": [ 110.684187623673708, -7.079410910547644 ] } },
+{ "type": "Feature", "properties": { "Swasta": "187", "Negeri": "1771" }, "geometry": { "type": "Point", "coordinates": [ 110.62586180469701, -7.045460060695538 ] } },
+{ "type": "Feature", "properties": { "Swasta": "260", "Negeri": "958" }, "geometry": { "type": "Point", "coordinates": [ 110.652848377656369, -7.095080533556308 ] } },
+{ "type": "Feature", "properties": { "Swasta": "854", "Negeri": "5983" }, "geometry": { "type": "Point", "coordinates": [ 110.880928445893602, -7.084634118217198 ] } }
+]
+}
